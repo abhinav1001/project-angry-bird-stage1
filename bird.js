6fc5730fe2507de590762d271c060d86e717b184
abhinav1001 <67742932+abhinav1001@users.noreply.github.com>
2020-07-16 20:01:47 +0530

    Add files via upload

---
+++ bird.js
@@ -0,0 +1,25 @@
+class Bird {
+    constructor(x, y) {
+      var options = {
+          'restitution':0.8,
+          'friction':1.0,
+          'density':1.0
+      }
+      this.body = Bodies.circle(x, y,50, options);
+      this.radius = 20;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      pos.x=mouseX;
+      pos.y=mouseY;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      ellipseMode(RADIUS);
+      fill("red");
+      ellipse(0, 0, this.radius,20);
+      pop();
+    }
+  };
---
+++ pig.js
@@ -0,0 +1,25 @@
+class pigs {
+    constructor(x, y) {
+      var options = {
+          'restitution':0.8,
+          'friction':1.0,
+          'density':1.0
+      }
+      this.body = Bodies.circle(x, y,30, options);
+      this.radius = 30;
+      
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      ellipseMode(RADIUS);
+      fill("green");
+      ellipse(0, 0, this.radius,30);
+      pop();
+    }
+  };
